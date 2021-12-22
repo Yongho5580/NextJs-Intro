99639436d7d5ed63190e3c8d1cff6c58e5138719
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2021-12-22 17:50:39 +0900

    first commit

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import NavBar from "../components/NavBar";
 
-export default function Home () {
+export default function About () {
     return (
         <div>
-            <h1> Hi NextJs</h1>
+            <h1>About Page</h1>
             <NavBar />
         </div>
     )
